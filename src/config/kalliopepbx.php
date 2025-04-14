3dcaddc9a835bc71cbdf9397ecbbd0a03ae3398d
Flavio Barachino <flavio.barachino@gmail.com>
2025-04-14 16:46:50 +0200

    Update kalliopepbx.php

--- src/config/kalliopepbx.php
+++ src/config/kalliopepbx.php
@@ -3,7 +3,7 @@
 return [
     'address'=>'voip.mypbx.com', // The address of the PBX server (also IP)
     'port'=>':443', // The port of the PBX server
-    'protocol'=>'https', // The protocol to use (http or https)
+    'protocol'=>'https://', // The protocol to use (http or https)
     'username'=>'kalliope', // The username for authentication
     'password'=>'kalliope', // The password for authentication
 ];
